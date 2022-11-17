81e94d1b81705a70e3f3baafb2c0f90ba7993fd0
Jimmy Leta <jimmy.leta@gmail.com>
2022-11-16 23:39:33 -0500

    Committing progress

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { doc, collection, addDoc, getFirestore, updateDoc, getDocs, getDoc } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAeZdcYt6MKmhRef2QwVYr33GJZ258cKsM",
@@ -10,27 +8,74 @@ const firebaseConfig = {
   storageBucket: "knock-it-out.appspot.com",
   messagingSenderId: "600141360369",
   appId: "1:600141360369:web:ff1fb41ba1c0673f0e0d50",
-  measurementId: "G-09MJF8VK52",
+  measurementId: "G-09MJF8VK52"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+import ko from 'knockout';
+
+function ToDoItem(task, isCompleted) {
+  this.task = ko.observable(task);
+  this.isCompleted = ko.observable(isCompleted);
+}
+
+function ListViewModel() {
+  var self = this;
+
+  const docRef = doc(db, "todos", "iGWnr6GGZgrTHiikeC4N");
+    
+  self.toDoItems = ko.observableArray([
+    new ToDoItem("Walk the dog", false),
+    new ToDoItem("Do more stuff", false),
+    new ToDoItem("Some stuff that's done", true),
+  ]);
+
+  self.addTodoItem = function () {
+    self.toDoItems.push(new ToDoItem("", false));
+  };
+
+  self.removeTodoItem = function (todoItem) {
+    self.toDoItems.remove(todoItem);
+  };
+
+  // self.loadTodoItems = async function() {
+  //   const querySnapshot = await getDoc(docRef);    
+  //   const data = querySnapshot.data().data;
+  //   console.log(this.toDoItems());
+  //   const todos = data.map(item => new ToDoItem(item.description, item.completed));
+  //   self.toDoItems = ko.observableArray(todos);
+  // }
+
+  self.saveTodoItems = async function() {
+    console.log(self.toDoItems());
+    try {
+      await updateDoc(docRef, {
+        data: [
+          {
+            description: "this is a task",
+            completed: true
+          },
+          {
+            description: "this is another task",
+            completed: false
+          }
+        ]
+      }, {merge: true}
+      );
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    
+  };
+}
+
+ko.applyBindings(new ListViewModel());
