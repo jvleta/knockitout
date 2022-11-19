c2fcdcd000ed7d11f20288df168ddd60e11e7762
Jimmy Leta <jimmy.leta@gmail.com>
2022-11-19 12:36:44 -0500

    Populate todo items with contents of firestore collection

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "firebase/app";
-import { doc, collection, addDoc, getFirestore, updateDoc, getDocs, getDoc } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  addDoc,
+  getFirestore,
+  updateDoc,
+  getDocs,
+  getDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAeZdcYt6MKmhRef2QwVYr33GJZ258cKsM",
@@ -8,19 +16,16 @@ const firebaseConfig = {
   storageBucket: "knock-it-out.appspot.com",
   messagingSenderId: "600141360369",
   appId: "1:600141360369:web:ff1fb41ba1c0673f0e0d50",
-  measurementId: "G-09MJF8VK52"
+  measurementId: "G-09MJF8VK52",
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
 
-
-import ko from 'knockout';
+import ko from "knockout";
 
 function ToDoItem(task, isCompleted) {
   this.task = ko.observable(task);
@@ -31,10 +36,21 @@ function ListViewModel() {
   var self = this;
 
   const docRef = doc(db, "todos", "iGWnr6GGZgrTHiikeC4N");
-    
-  self.toDoItems = ko.observableArray([
-  ]);
-  
+
+  self.loadTodoItems = function () {
+    const docRef = doc(db, "todos", "iGWnr6GGZgrTHiikeC4N");
+    let todos = ko.observableArray([]);
+    getDoc(docRef).then((querySnapshot) => {
+      const data = querySnapshot.data().data;
+      console.log(this.toDoItems());
+      data.forEach((item) => {
+        todos.push(new ToDoItem(item.description, item.completed));
+      });
+    });
+
+    return todos;
+  };
+
   self.addTodoItem = function () {
     self.toDoItems.push(new ToDoItem("", false));
   };
@@ -43,31 +59,26 @@ function ListViewModel() {
     self.toDoItems.remove(todoItem);
   };
 
-  self.loadTodoItems = async function() {
-    const querySnapshot = await getDoc(docRef);    
-    const data = querySnapshot.data().data;
-    console.log(this.toDoItems());
-    this.toDoItems.removeAll();
-
-    data.forEach(item => {
-      self.toDoItems.push(new ToDoItem(item.description, item.completed));
-    });
-  }
-
-  self.saveTodoItems = async function() {
+  self.saveTodoItems = async function () {
     console.log(self.toDoItems());
 
     try {
-      await updateDoc(docRef, {
-        data: self.toDoItems().map(item => { return {completed: item.isCompleted(), description: item.task()}})
-      }, {merge: true}
+      await updateDoc(
+        docRef,
+        {
+          data: self.toDoItems().map((item) => {
+            return { completed: item.isCompleted(), description: item.task() };
+          }),
+        },
+        { merge: true }
       );
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
-    
   };
+
+  self.toDoItems = self.loadTodoItems();
 }
 
 ko.applyBindings(new ListViewModel());
